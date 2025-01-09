1cf3b49ec3992d74071adb508330fbe6f9f93cf3
Philipp Schlaug <30334541+phschlaug@users.noreply.github.com>
2025-01-09 21:47:08 +0100

    Remove not needed platforms

--- src/maui_app/BartenderCalculator/BartenderCalculator/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace BartenderCalculator;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
